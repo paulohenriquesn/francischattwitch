13ffee5e5606505cd5a4abb3b21ef845eb307dd4
Paulo Henrique <gladgamer01@gmail.com>
2020-06-02 17:28:05 -0300

    Updates

---
+++ precache-manifest.9c04e1732e55c787e17ce500024c0917.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8bca22acfb5db61ce628712ca089cf0",
+    "url": "/francischattwitch/index.html"
+  },
+  {
+    "revision": "76cb4118cd2db14f26e5",
+    "url": "/francischattwitch/static/css/main.3452e7a4.chunk.css"
+  },
+  {
+    "revision": "2696acc22fa306c915e7",
+    "url": "/francischattwitch/static/js/2.093396a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/francischattwitch/static/js/2.093396a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76cb4118cd2db14f26e5",
+    "url": "/francischattwitch/static/js/main.2e941f5b.chunk.js"
+  },
+  {
+    "revision": "8a821db433f2676fd916",
+    "url": "/francischattwitch/static/js/runtime-main.ca7c5a5e.js"
+  },
+  {
+    "revision": "d46843afdeb3c486ed4d493099a18ee7",
+    "url": "/francischattwitch/static/media/chat.d46843af.png"
+  }
+]);

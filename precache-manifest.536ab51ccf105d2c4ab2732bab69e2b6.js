58f08b5a73ca56d285b66e53e1e1a339d8eb1d9c
Paulo Henrique <gladgamer01@gmail.com>
2020-06-02 17:19:14 -0300

    Add files via upload

---
+++ precache-manifest.536ab51ccf105d2c4ab2732bab69e2b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7969e6cf949c73c5feea232406dde9",
+    "url": "/paulohenriquesn/francischattwitch/index.html"
+  },
+  {
+    "revision": "a671dc0cb0d1f0a1f63b",
+    "url": "/paulohenriquesn/francischattwitch/static/css/main.3452e7a4.chunk.css"
+  },
+  {
+    "revision": "2696acc22fa306c915e7",
+    "url": "/paulohenriquesn/francischattwitch/static/js/2.093396a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paulohenriquesn/francischattwitch/static/js/2.093396a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a671dc0cb0d1f0a1f63b",
+    "url": "/paulohenriquesn/francischattwitch/static/js/main.a5f3aa1e.chunk.js"
+  },
+  {
+    "revision": "7e989c036791d1332f32",
+    "url": "/paulohenriquesn/francischattwitch/static/js/runtime-main.c4a83b17.js"
+  },
+  {
+    "revision": "d46843afdeb3c486ed4d493099a18ee7",
+    "url": "/paulohenriquesn/francischattwitch/static/media/chat.d46843af.png"
+  }
+]);
